da0bbbb84bb10bd4e19e831d5a759ad5e4c4b97a
ZamIzzudin <azzamizzudinhasan@gmail.com>
2026-01-14 10:33:04 +0700

    fix:(redirect get ready button hardcode)

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -3,8 +3,18 @@
 import { useRouter } from "next/navigation";
 import Button from "./atomic/button";
 
+import { SocmedDataProps } from "@/types/socmed";
+import { useSocmed } from "@/services/socmed/hook";
+
 export default function CTA() {
   const router = useRouter();
+
+  const { data: socmedData = [], isLoading: socmedLoading } = useSocmed();
+
+  const contactList = socmedData.find(
+    (item: SocmedDataProps) =>
+      item.socmed_link && ["WHATSAPP"].includes(item.socmed_name)
+  )?.socmed_link;
   return (
     <div className="w-full px-[5%] md:px-[7%] lg:px-[10%] text-center py-[10%] md:py-0 md:pb-[5%] space-y-5 md:space-y-8 flex flex-col items-center">
       <h2 className="text-[32px] md:text-[40px] lg:text-[49px] font-semibold">
@@ -14,7 +24,7 @@ export default function CTA() {
         Get the full proposal and training details by clicking the button below.
       </p>
       <Button
-        onClick={() => router.push("https://wa.me/62895805254925")}
+        onClick={() => router.push(`https://wa.me/${contactList}`)}
         label="Request Proposal"
         rounded
         type="primary"
